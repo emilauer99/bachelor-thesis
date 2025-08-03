b529afdf61b699cc1e2c2d186770c2d6c6ebc624
Emil Auer <e.auer@itkaufmann.at>
2025-08-03 13:58:24 +0200

    implement mobile adjustments for angular

--- angular_app/src/app/components/home/customers/customers.component.ts
+++ angular_app/src/app/components/home/customers/customers.component.ts
@@ -8,11 +8,12 @@ import {CustomNotificationService} from '../../../services/custom-notification.s
 import {CustomerDialogComponent} from './customer-dialog/customer-dialog.component';
 import {MatList, MatListItem} from '@angular/material/list';
 import {SpinnerComponent} from '../../utils/spinner/spinner.component';
-import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatButton, MatFabButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {environment} from '../../../../environments/environment';
 import {IProjectDataProvider, PROJECT_DATA} from '../../../services/providers/projects.provider';
 import {CUSTOMER_DATA, ICustomerDataProvider} from '../../../services/providers/customers.provider';
+import {MobileService} from '../../../services/mobile.service';
 
 @Component({
   selector: 'app-customers',
@@ -23,6 +24,7 @@ import {CUSTOMER_DATA, ICustomerDataProvider} from '../../../services/providers/
     MatList,
     MatListItem,
     SpinnerComponent,
+    MatFabButton,
   ],
   templateUrl: './customers.component.html',
   standalone: true,
@@ -31,6 +33,7 @@ import {CUSTOMER_DATA, ICustomerDataProvider} from '../../../services/providers/
 export class CustomersComponent {
   constructor(@Inject(CUSTOMER_DATA) public customerService: ICustomerDataProvider,
               private notificationService: CustomNotificationService,
+              public mobileService: MobileService,
               private dialog: MatDialog) {
     if(!this.customerService.customers())
       this.customerService.getAll()
